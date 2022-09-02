d4498a14fc8452c16c5d9a37c28ecb799d6ecf89
nyubblue <thanhbuynitu@gmail.com>
2022-09-03 06:13:13 +0700

    C1

---
+++ api_parameter.py
@@ -0,0 +1,21 @@
+from datetime import *
+
+import requests
+
+parameters = {
+    "lat": 53.282570,
+    "lng": -4.631860,
+    "formatted": 0,
+}
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now)
